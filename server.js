9ec2585d01cb0700fe789efc96948cc646468ae1
slenksp <slenksp@github.com>
2020-01-15 21:02:54 -0600

    removed another variable

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var exphbs = require("express-handlebars");
 app.use(express.urlencoded({
     extended: false
 }));
